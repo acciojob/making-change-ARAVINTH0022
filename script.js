c09c349c227c80b23fb2f88c8ba61efe7c24fb41
ARAVINTH0022 <113502178+ARAVINTH0022@users.noreply.github.com>
2026-08-10 00:20:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-const makeChange = (c) => {
-  // your name here
-};
+function makeChange(cents) {
+    let q = Math.floor(cents / 25);
+    cents %= 25;
 
-// Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+    let d = Math.floor(cents / 10);
+    cents %= 10;
+
+    let n = Math.floor(cents / 5);
+    cents %= 5;
+
+    let p = cents;
+
+    return {
+        q: q,
+        d: d,
+        n: n,
+        p: p
+    };
+}
